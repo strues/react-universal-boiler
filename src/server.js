4ef3139f64d42ebd29db2559b9cfece8a3e04786
Steven Truesdell <steven@strues.io>
2016-10-01 11:24:50 -0600

    add source-map-support for server.

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+// Useful for debugging node.js code that has been bundled with Webpack.
+// https://github.com/evanw/node-source-map-support
+import 'source-map-support/register';
+
 import http from 'http';
 import path from 'path';
 // Express deps
